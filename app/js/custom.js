4b5447ea099865056614fe7675d474b4e9e29a95
Yuriy Bogatyrenko <yurabogatyrenko@gmail.com>
2016-10-26 19:52:46 +0600

    icons page inserted

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,5 +1,22 @@
 var doc = $(document);
+var menu_activated = false;
 
 doc.ready(function(){
    $('select.selectpicker').selectpicker();
+   doc.on('click', '[data-call-submenu]', function (e) {
+      var _this = $(this);
+      var bl = $('[data-submenu="' + _this.attr("data-call-submenu") + '"]');
+
+      if(!_this.hasClass('active')) {
+         _this.addClass('active');
+         bl.show().addClass('active');
+      } else {
+         _this.removeClass('active');
+         bl.hide().removeClass('active');
+      }
+
+
+      menu_activated = true;
+      e.preventDefault();
+   });
 });
